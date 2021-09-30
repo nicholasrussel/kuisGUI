d965f9d4965261ac8524bbe0e83b088561c1fe10
Nicholas Russel <Nicholas Russel@LAPTOP-E1C6PSRA>
2021-09-30 15:01:50 +0700

    add class kehadiran

--- src/model/Kehadiran.java
+++ src/model/Kehadiran.java
@@ -9,8 +9,40 @@ package model;
  *
  * @author Nicholas Russel
  */
-public interface Kehadiran {
+public class Kehadiran {
+    private int status;
+    private String tanggal;
+    private int totalJamMasuk;
 
-    final int HADIR = 1;
-    final int ALPHA = 0;
+    public Kehadiran(int status, String tanggal, int totalJamMasuk) {
+        setStatus(status);
+        setTanggal(tanggal);
+        setTotalJamMasuk(totalJamMasuk);
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public String getTanggal() {
+        return tanggal;
+    }
+
+    public void setTanggal(String tanggal) {
+        this.tanggal = tanggal;
+    }
+
+    public int getTotalJamMasuk() {
+        return totalJamMasuk;
+    }
+
+    public void setTotalJamMasuk(int totalJamMasuk) {
+        this.totalJamMasuk = totalJamMasuk;
+    }
+    
+    
 }
